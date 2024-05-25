10fab5003d4469164fa2012ef56675f63f97134f
Kari Taylor. <klg56880@gmail.com.>
2024-05-25 18:14:40 -0500

    csv files added

--- db_operations_karitaylor.py
+++ db_operations_karitaylor.py
@@ -4,4 +4,14 @@ Author: Kari Taylor
 Objective: Create a Python script that demonstrates the ability to interact with a SQL database, including creating a database, defining a schema, and executing various SQL commands. Incorporate logging to document the process and provide user feedback.
 '''
 
+# Import Standard Dependencies
+import csv
+import pathlib
+import sqlite3
+import uuid
+
+# Import Local Dependencies
+import pandas
+import pyarrow
+
 
