2d3cdf4edcad2a8321eda35a5a6098d811bc87bf
Kari Taylor. <klg56880@gmail.com.>
2024-05-25 10:03:30 -0500

    add .gitignore, cmds to readme

--- db_operations_karitaylor.py
+++ db_operations_karitaylor.py
@@ -1,1 +1,7 @@
+'''
+Module 5 SQL Project
+Author: Kari Taylor
+Objective: Create a Python script that demonstrates the ability to interact with a SQL database, including creating a database, defining a schema, and executing various SQL commands. Incorporate logging to document the process and provide user feedback.
+'''
+
 
